d661b661b47f5414cc1d137dc67470c0d414573d
zzanek <151283905+zzanek@users.noreply.github.com>
2024-02-16 10:26:01 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 /// alerthi.js
 javascript:alert("hi")
-/// exttest.js
+/// ext.js
+javascript:(function() {
+    var extensionId = prompt('Enter the extension ID:');
+    if (extensionId) {
+        location.href = 'https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=' + (navigator.appVersion.match(/Chrome\/(\S+)/)[1]) + '&x=id%3D' + (extensionId) + '%26installsource%3Dondemand%26uc';
+    } else {
+        alert('No extension ID provided.');
+    }
+})();
+/// extension.js
