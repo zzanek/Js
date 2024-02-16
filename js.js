76437043172b2fcd9550976e17870e15a0619cbb
zzanek <151283905+zzanek@users.noreply.github.com>
2024-02-16 12:58:47 -0800

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,10 @@
-
+javascript:(function(){
+    var bookmarkletCode = prompt('Bookmarklet:');
+    if(bookmarkletCode) {
+        try {
+            eval(bookmarkletCode);
+        } catch(error) {
+            alert('Error: ' + error);
+        }
+    }
+})();
