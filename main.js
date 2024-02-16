78d42bbd6036e56a2343047291e5c3c08bcc4cd5
zzanek <151283905+zzanek@users.noreply.github.com>
2024-02-16 10:30:20 -0800

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,5 @@ javascript:(function() {
     location.href = 'https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=' + (navigator.appVersion.match(/Chrome\/(\S+)/)[1]) + '&x=id%3D' + (location.pathname.split('/').pop()) + '%26installsource%3Dondemand%26uc';
 })();
 /// test.js
+/// willthis.js
+alert("hi")
