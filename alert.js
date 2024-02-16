936f14314eea155c81cd29c722875354a7f3c6aa
zzanek <151283905+zzanek@users.noreply.github.com>
2024-02-15 17:01:49 -0800

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,5 +1,2 @@
 /// alert.js
-javascript:(function() {
-    location.href = 'https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=' + (navigator.appVersion.match(/Chrome\/(\S+)/)[1]) + '&x=id%3D' + (location.pathname.split('/').pop()) + '%26installsource%3Dondemand%26uc';
-})();
-
+javascript:alert("hi")
