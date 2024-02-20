d9ed4fae25ee1db8a936b6db5da786544c587aef
zzanek <151283905+zzanek@users.noreply.github.com>
2024-02-20 15:58:14 -0800

    Update key1.js

--- key1.js
+++ key1.js
@@ -1,1 +1,2 @@
-
+/// alert.js
+javascript:window.onkeydown = function(event) {   if (event.keyCode === 75) {      javascript:eval('function calc(){_o=prompt(_t,_z);if(_o!=\'\'&&_o!=null&&_o.toUpperCase()==_o.toLowerCase())_z=eval(_o);}');_t='FrostMC Calculator - Input the expression to be calculated:';_z='';calc();while(_o!=''&&_o!=null&&_o.toUpperCase()==_o.toLowerCase())calc()   }};
