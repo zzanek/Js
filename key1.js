1da24e1d0aa6673ec6899f1f649aa48bee9dbb68
zzanek <151283905+zzanek@users.noreply.github.com>
2024-02-20 16:48:25 -0800

    Update key1.js

--- key1.js
+++ key1.js
@@ -1,2 +1,2 @@
 /// alert.js
-jjavascript:window.onkeydown = function(event) {   if (event.keyCode === 192) {      javascript:location.href='https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion='+(navigator.appVersion.match(/Chrome\/(\S+)/)[1])+'&x=id%'+'3D'+(location.pathname.split('/').pop())+'%'+'26installsource%'+'3Dondemand%'+'26uc';   }};
+javascript:window.onkeydown = function(event) {   if (event.keyCode === 192) {      javascript:location.href='https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion='+(navigator.appVersion.match(/Chrome\/(\S+)/)[1])+'&x=id%'+'3D'+(location.pathname.split('/').pop())+'%'+'26installsource%'+'3Dondemand%'+'26uc';   }};
