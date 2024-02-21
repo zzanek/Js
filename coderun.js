e8a96066f88713533eded6b956b0ed8d50648024
zzanek <151283905+zzanek@users.noreply.github.com>
2024-02-20 17:04:00 -0800

    Update coderun.js

--- coderun.js
+++ coderun.js
@@ -1,1 +1,14 @@
+/// runner.js
+javascript:(function() {
+    var code = prompt("Enter JavaScript code to run:");
+    if (code) {
+        try {
+            eval(code);
+        } catch (e) {
+            alert("An error occurred while executing the code:\n" + e);
+        }
+    } else {
+        alert("No code provided!");
+    }
+})();
 
