3c35b27261fb28b2b1a3926404f28f678075870f
zzanek <151283905+zzanek@users.noreply.github.com>
2024-05-03 11:53:02 -0700

    Update coderun.js

--- coderun.js
+++ coderun.js
@@ -1,2 +1,2 @@
 /// runner.js
-javascript:window.onkeydown = function(event) {   if (event.keyCode === 192) {      javascript:(function(){var code=prompt("Enter JavaScript code to run:");if(code){var script=document.createElement('script');script.textContent=code;document.body.appendChild(script);}else{alert("No code.");}})();   }};
+javascript: window.onkeydown = function(event) { if (event.keyCode === 192) { javascript: (function() { var code = prompt("Enter JavaScript code to run:"); if (code && code.trim()) { var script = document.createElement('script'); script.textContent = code; document.body.appendChild(script); } })(); }};
